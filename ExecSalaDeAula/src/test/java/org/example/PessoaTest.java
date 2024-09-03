b36bedef534f1f13aa9ccc193eb6ca508a343239
PEDRO ABRAHAO BARROS <pedro.barros3@GRANBERY.local>
2024-09-03 20:17:57 -0300

    Codigo atualizado + Casos de testes

--- ExecSalaDeAula/src/test/java/org/example/PessoaTest.java
+++ ExecSalaDeAula/src/test/java/org/example/PessoaTest.java
@@ -7,14 +7,32 @@ class PessoaTest {
 
 
     @Test
-    void deveRetornarSalarioEnsinoMedio() {
+    void deveRetornarSalarioAdminEnsinoMedio() {
         Pessoa admin = new Administrativo("Jean", Escolaridade.ENSINO_MEDIO);
         assertEquals(1000.0, admin.calcularSalario(), "Salário do Administrativo deve ser 1000.0");
     }
     @Test
-    void deveRetornarSalarioAdministrativo() {
+    void deveRetornarSalarioAdminGraduacao() {
         Pessoa admin = new Administrativo("João", Escolaridade.GRADUACAO);
         assertEquals(1000.0, admin.calcularSalario(), "Salário do Administrativo deve ser 1000.0");
+    }
+
+    @Test
+    void deveRetornarSalarioAdminMestrado() {
+        Pessoa admin = new Administrativo("João", Escolaridade.MESTRADO);
+        assertEquals(1000.0, admin.calcularSalario(), "Salário do Administrativo deve ser 1000.0");
+    }
+
+    @Test
+    void deveRetornarSalarioAdminDoutorado() {
+        Pessoa admin = new Administrativo("João", Escolaridade.DOUTORADO);
+        assertEquals(1000.0, admin.calcularSalario(), "Salário do Administrativo deve ser 1000.0");
+    }
+
+    @Test
+    void deveRetornarSalarioCoordenadorComEnsinoMedio() {
+        Pessoa coordenador = new Coordenador("Maria", Escolaridade.ENSINO_MEDIO);
+        assertEquals(1000.0, coordenador.calcularSalario(), "Salário do Coordenador com Graduação deve ser 1000.0");
     }
 
     @Test
@@ -24,8 +42,38 @@ class PessoaTest {
     }
 
     @Test
+    void deveRetornarSalarioCoordenadorComMestrado() {
+        Pessoa coordenador = new Coordenador("Maria", Escolaridade.MESTRADO);
+        assertEquals(1200.0, coordenador.calcularSalario(), "Salário do Coordenador com Graduação deve ser 1200.0");
+    }
+
+    @Test
+    void deveRetornarSalarioCoordenadorComDoutorado() {
+        Pessoa coordenador = new Coordenador("Maria", Escolaridade.DOUTORADO);
+        assertEquals(1300.0, coordenador.calcularSalario(), "Salário do Coordenador com Graduação deve ser 1300.0");
+    }
+
+    @Test
+    void deveRetornarSalarioProfessorComEnsinoMedio() {
+        Pessoa professor = new Professor("Carlos", Escolaridade.ENSINO_MEDIO, 10);
+        assertEquals(10000.0, professor.calcularSalario(), "Salário do Professor com Doutorado e 10 aulas deve ser 10000.0");
+    }
+
+    @Test
+    void deveRetornarSalarioProfessorComGraduacao() {
+        Pessoa professor = new Professor("Carlos", Escolaridade.GRADUACAO, 10);
+        assertEquals(11000.0, professor.calcularSalario(), "Salário do Professor com Doutorado e 10 aulas deve ser 11000.0");
+    }
+
+    @Test
+    void deveRetornarSalarioProfessorComMestrado() {
+        Pessoa professor = new Professor("Carlos", Escolaridade.MESTRADO, 10);
+        assertEquals(12000.0, professor.calcularSalario(), "Salário do Professor com Doutorado e 10 aulas deve ser 12000.0");
+    }
+
+    @Test
     void deveRetornarSalarioProfessorComDoutorado() {
-        Pessoa professor = new Professor("Carlos", Escolaridade.DOUTORADO, 6);
-        assertEquals(7800.0, professor.calcularSalario(), "Salário do Professor com Doutorado e 10 aulas deve ser 7800.0");
+        Pessoa professor = new Professor("Carlos", Escolaridade.DOUTORADO, 10);
+        assertEquals(13000.0, professor.calcularSalario(), "Salário do Professor com Doutorado e 10 aulas deve ser 13000.0");
     }
 }
